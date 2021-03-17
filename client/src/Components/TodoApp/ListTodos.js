73a4074ed2285d0f7a56cdbc6051620d1a83e641
Shiwei He <irene@Shiweis-MacBook-Air.local>
2021-03-17 17:49:48 -0400

    todolist with fake data+add style to list

--- client/src/Components/TodoApp/ListTodos.js
+++ client/src/Components/TodoApp/ListTodos.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 export default function ListTodos({match}) {
+    
+    const todos=[
+        {id:1, description:'Learn React'},
+        {id:2, description:'Learn Java-Spring'},
+        {id:3, description:'Doctort appointment this Sunday'},
+        {id:4, description:'Health insurance application'},
+    ]
     return (
         <div className="todo-body">
-            List of Todos for {match.params.name}
+           <h1>List of Todos for {match.params.name}</h1> 
+           <ul className="list-body">
+               {todos.map((todo)=>(
+               <li key={todo.id}>
+                   {todo.description}
+               </li>
+           ))}
+           </ul>
+           
+           
         </div>
     )
 }
